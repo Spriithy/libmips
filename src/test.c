c3947a5ee9a3650068f5bf476428fe7ac78c97dd
theophile <theophile@localhost.localdomain>
2017-10-26 18:45:43 +0200

    clean repo

--- src/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "builtins/gen.h"
-#include "instruction.h"
-#include "trace.h"
-
-int main() {
-  instr_t i = GEN_SYSCALL();
-  trace(i);
-}
--- src/trace.h
+++
@@ -1,8 +0,0 @@
-#ifndef MIPS_TRACE_H
-#define MIPS_TRACE_H
-
-#include "instruction.h"
-
-void trace(instr_t i);
-
-#endif  // MIPS_TRACE_H
